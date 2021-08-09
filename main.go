7316557a1faaf3261cf7be6a0651b94493c3bbe4
Bamimore-Tomi <tomibami2020@gmail.com>
2021-08-09 03:47:35 -0400

    initial

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"strconv"
+	"time"
+)
+
+func scanPort(protocol, hostname string, port int) bool {
+	address := hostname + ":" + strconv.Itoa(port)
+	conn, err := net.DailTimeout(protocol, address, 60*time.Second)
+	if err != nil {
+		return false
+	}
+	defer conn.Close()
+	return true
+}
+
+func main() {
+	fmt.Println("Port Scanning")
+	open := scanPort("tcp", "localhost", 1313)
+	fmt.Printf("Port Open: %t\n", open)
+}
